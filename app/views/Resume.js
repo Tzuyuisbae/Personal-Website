cccc57744cd0500081272187a32569e51f9a546c
joe liu <joeliu@joes-Air.home>
2019-03-15 23:02:08 -0400

    resize resume on window resize

--- app/views/Resume.js
+++ app/views/Resume.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import Helmet from 'react-helmet';
 
@@ -15,23 +15,66 @@ import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
 import { skills, categories } from '../data/resume/skills';
 
+var width;
 
-const Resume = () => (
-  <Main>
-    <Helmet title="Resume" />
-    <article className="post" id="resume">
-      <header>
-        <div className="title">
-          <h2><Link to="resume">Resume</Link></h2>
-          <iframe src="https://drive.google.com/file/d/1X036l3H9Ye-gd1TxsKf9TcUv5tzfnajP/preview" width={document.documentElement.clientWidth/2} height={document.documentElement.clientHeight*1.15}></iframe>        
-          <br/>
-          <a href="https://drive.google.com/open?id=1X036l3H9Ye-gd1TxsKf9TcUv5tzfnajP" target = "_blank"> Download </a>
-          </div>
-      </header>
+export default class Resume extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  updateWindowDimensions() {
+    width = window.innerWidth;
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  render(){
+    return(
+      <Main>
+      <Helmet title="Resume" />
+      <article className="post" id="resume">
+        <header>
+          <div className="title">
+            <h2><Link to="resume">Resume</Link></h2>
+            <iframe src="https://drive.google.com/file/d/1X036l3H9Ye-gd1TxsKf9TcUv5tzfnajP/preview" width={width/2} height={document.documentElement.clientWidth/1.5}></iframe>        
+            <br/>
+            <a href="https://drive.google.com/open?id=1X036l3H9Ye-gd1TxsKf9TcUv5tzfnajP" target = "_blank"> Download </a>
+            </div>
+        </header>
+      
+
+      </article>
+    </Main>
+    )
+  }
+}
+// const Resume = () => (
+  
+//   <Main>
+//     <Helmet title="Resume" />
+//     <article className="post" id="resume">
+//       <header>
+//         <div className="title">
+//           <h2><Link to="resume">Resume</Link></h2>
+//           <iframe src="https://drive.google.com/file/d/1X036l3H9Ye-gd1TxsKf9TcUv5tzfnajP/preview" width={width} height={document.documentElement.clientWidth/1.5}></iframe>        
+//           <br/>
+//           <a href="https://drive.google.com/open?id=1X036l3H9Ye-gd1TxsKf9TcUv5tzfnajP" target = "_blank"> Download </a>
+//           </div>
+//       </header>
     
 
-    </article>
-  </Main>
-);
+//     </article>
+//   </Main>
+// );
 
-export default Resume;
+// export default Resume;
