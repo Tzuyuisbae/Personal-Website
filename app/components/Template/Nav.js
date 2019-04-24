62aecb8567627006fe335b18ba828b4c5a4bd04f
joe liu <joeliu@joes-MacBook-Air.local>
2019-04-24 11:10:01 -0400

    added text animation to front page

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -1,45 +1,83 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import ReactRevealText from 'react-reveal-text';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import SplitText from 'react-pose-text';
 
 import data from '../../data/contact';
 
-const Nav = () => (
-  <section id="sidebar">
-    <section id="intro">
-      <Link to="/" className="logo">
-        <img src={`${BASE_PATH}/images/me_icon.png`} alt="" />
-      </Link>
-      <header>
-        <h2>Joe Liu</h2>
-        <p><a href="mailto:liujoe99@hotmail.ca">liujoe99@hotmail.ca</a></p>
-      </header>
-    </section>
+const charPoses = {
+  exit: { opacity: 0, y: 20 },
+  enter: {
+    opacity: 1,
+    y: 0,
+    delay: ({ charIndex }) => charIndex * 30
+  }
+};
 
-    <section className="blurb">
-      <h2>About</h2>
-      <p>2nd Year Computer Science student at UTSC <br/> 
-        Full Stack Developer for UTSC <br/>
-        TA for Introduction to Computer Science II (CSCA48) at UTSC
+class Nav extends React.Component {
 
-      </p>
-      <ul className="actions">
-        <li>
-          {window.location.pathname !== `${BASE_PATH}/resume` ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
-        </li>
-      </ul>
-    </section>
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
 
-    <section id="footer">
-      <ul className="icons">
-        {data.map(s => (
-          <li key={s.label}><a href={s.link}><FontAwesomeIcon icon={s.icon} /></a></li>
-        ))}
-      </ul>
-    </section>
-  </section>
-);
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 100);
+  }
+  render(){
+
+    return(
+      <section id="sidebar">
+      <section id="intro">
+        <Link to="/" className="logo">
+          <img src={`${BASE_PATH}/images/me_icon.png`} alt="" />
+        </Link>
+        <header>
+          <h2>
+          <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+              Joe Liu
+
+            </SplitText>
+          </h2>
+          <div className="fancyText"> 
+            <a href="mailto:liujoe99@hotmail.ca">
+            <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+
+              liujoe99@hotmail.ca
+            </SplitText>
+            </a>
+          </div>
+        </header>
+      </section>
+
+      <section className="blurb">
+        <h2>About</h2>
+        <p>2nd Year Computer Science student at UTSC <br/> 
+          Full Stack Developer for UTSC <br/>
+          TA for Introduction to Computer Science II (CSCA48) at UTSC
+
+        </p>
+        <ul className="actions">
+          <li>
+            {window.location.pathname !== `${BASE_PATH}/resume` ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+          </li>
+        </ul>
+      </section>
+
+      <section id="footer">
+        <ul className="icons">
+          {data.map(s => (
+            <li key={s.label}><a href={s.link}><FontAwesomeIcon icon={s.icon} /></a></li>
+          ))}
+        </ul>
+      </section>
+      </section>
+    );
+  
+  }
+}
 
 export default Nav;
--- app/views/Index.js
+++ app/views/Index.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import ReactRevealText from 'react-reveal-text';
 import Main from '../layouts/Main';
 
-const Index = () => (
-  <Main>
-    <article className="post" id="index">
-      <header>
-        <div className="title">
-          <h2><Link to="/">About this site</Link></h2>
-          <p>A beautiful, responsive, react app written with modern Javascript.</p>
-        </div>
-      </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
-      </p>
-      <p> Website template forked from <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
-    </article>
-  </Main>
-);
+class Index extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 100);
+  }
+
+  render(){
+    return(
+      <Main>
+      <article className="post" id="index">
+        <header>
+          <div className="title">
+            <h2><Link to="/">
+            <ReactRevealText show={this.state.show}>
+                About this site
+            </ReactRevealText>
+            </Link></h2>
+            <div className="fancyText">
+            
+              <ReactRevealText show={this.state.show}>
+                My Personal Website built with React
+              </ReactRevealText>
+            </div>
+
+          </div>
+        </header>
+
+        <p> 
+
+          Welcome to my website. Please feel free to read more {' '}
+          <Link to="/about"> about me</Link>, or you can check out my {' '}
+          <Link to="/resume">resume</Link>, {' '}
+          <Link to="/projects">projects</Link>, {' '}
+          view <Link to="/stats">site statistics</Link>, {' '}
+          or <Link to="/contact">contact</Link> me.
+        </p>
+        
+        <p> Website template forked from <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      </article>
+      </Main>
+    );
+  }
+}
 
 export default Index;
