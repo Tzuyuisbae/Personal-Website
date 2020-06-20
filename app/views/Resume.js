df08803ea27fafce9854e92ba87509cfc872cb09
Joe <liujoe99@hotmail.ca>
2020-06-20 14:57:22 -0400

    updated resume

--- app/views/Resume.js
+++ app/views/Resume.js
@@ -52,9 +52,9 @@ export default class Resume extends Component{
             <h2><Link to="resume">
             <ReactRevealText show={this.state.show}> Resume </ReactRevealText>
             </Link></h2>
-            <iframe src="https://drive.google.com/file/d/1FftT42RM9byZUyAcBVhBXmCiAZgx-9U4/preview" width={width/2} height={document.documentElement.clientWidth/1.5}></iframe>
+            <iframe src="https://drive.google.com/file/d/14kToS2Byh7wfupqjAIwXIqlQAXQ3_y3-/preview" width={width/2} height={document.documentElement.clientWidth/1.5}></iframe>
             <br/>
-            <a className="button" href='https://drive.google.com/file/d/1FftT42RM9byZUyAcBVhBXmCiAZgx-9U4' target="_blank"> Download </a>
+            <a className="button" href='https://drive.google.com/file/d/14kToS2Byh7wfupqjAIwXIqlQAXQ3_y3-' target="_blank"> Download </a>
             </div>
         </header>
 
